259b942c73fc8d99b5551e5bfd81fb3c871c824c
rakshya98 <rakshyatimalsena@gmail.com>
2023-06-01 22:06:51 +0545

    add delete and edit function

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,9 @@ from django.urls import path
 from main_app import views
 
 urlpatterns = [
-    path('',views.home,name='main_app-home')
+    path('',views.home,name='main_app-home'),
+    path('add/',views.add,name='main_app-add'),
+    path('delete/<int:id>',views.delete,name='main_app-delete'),
+    path('edit/<int:id>',views.edit,name='main_app-edit'),
+    path('delete-all',views.delete_all,name="main_app-deleteall")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from main_app.models import TODO
+
 
 # Create your views here.
 def home(request):
-    return render(request,'main_app/home.html')
+    todo=TODO.objects.all()
+    return render(request,'main_app/home.html',{'todo':todo})
 
+def add(request):
+    if request.method=='GET':
+        return render(request,'main_app/add-todo.html')
+    else:
+        title=request.POST['title']
+        desc=request.POST['desc']
+
+        TODO.objects.create(title=title,description=desc,is_completed=False, user_id=1)
+
+        return redirect('main_app-home')
+
+
+def delete(request,id):
+    TODO.objects.get(id=id).delete()
+    return redirect('main_app-home')                       
+
+def edit(request,id):
+    todo=TODO.objects.get(id=id)
+    if request.method=='GET':
+        return render(request,'main_app/edit-todo.html',{'todo':todo})
+    else:
+        title=request.POST['title']
+        desc=request.POST['desc']
+
+        todo.title=request.POST['title']
+        todo.description=request.POST['desc']
+        todo.save()
+        return redirect('main_app-home')
+
+
+def delete_all(request):
+    
+    TODO.objects.all().delete() 
+    return redirect('main_app-home')
