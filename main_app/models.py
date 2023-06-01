3c06340c2d89312300673b5f70c943c63ebcf7fd
rakshya98 <rakshyatimalsena@gmail.com>
2023-06-01 18:57:39 +0545

    initial commit

---
+++ main_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User 
+# Create your models here.
+class TODO(models.Model):
+    title=models.CharField(max_length=50)
+    description=models.CharField(max_length=100)
+    is_completed=models.BooleanField()
+    created_date=models.DateField(auto_now_add=True)  # object create garda aafei date time dinxa auto_now_add le , hamle date lehkiraakhnu parena
+    updated_date=models.DateField(auto_now=True) # everytime object is modified , aafei date dinxa
+    user=models.ForeignKey(User ,on_delete=models.CASCADE)
