9fe95e1c59405e4f23391ca3636b2c2958758390
rakshya98 <rakshyatimalsena@gmail.com>
2023-06-01 19:39:52 +0545

    add meta classs

--- main_app/models.py
+++ main_app/models.py
@@ -8,3 +8,10 @@ class TODO(models.Model):
     created_date=models.DateField(auto_now_add=True)  # object create garda aafei date time dinxa auto_now_add le , hamle date lehkiraakhnu parena
     updated_date=models.DateField(auto_now=True) # everytime object is modified , aafei date dinxa
     user=models.ForeignKey(User ,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        db_table='todo'
+        verbose_name_plural="TODOes"
